3591da1ce24812277e599d8b46a12760015f2e4c
Livo Kids <jewelpk528@gmail.com>
2026-05-15 04:50:32 +0600

    trigger AAB build

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.livokids.app',
+  appName: 'Livo Kids',
+  webDir: 'dist'
+};
+
+export default config;
